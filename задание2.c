2824358f3b6288f533ce435e556b07ee755d7396
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-11-29 18:00:07 +0300

    Create задание2.c

---
+++ задание2.c
@@ -0,0 +1,27 @@
+include <stdio.h> 
+#include <locale.h>
+#include <math.h> 
+#define N 10
+#define K 100
+int main() {
+	int A[K];
+	int temp;
+	int i,x;
+	float z,k1,k2,xx;
+	setlocale(0, "rus");
+	puts("Задна функция у = 1.8x^2 - sin10x и интервал[0.2;2.2]\n");
+		    scanf("%d", &temp);
+			for (int i = ; i < K; i++) {
+				A[i] = 1.8 * (float)pow(x, 2) - (float)sin(10 * x);
+				if (A[(int)x] >= 0) {
+					z += A[(int)x];
+					k1
+				}
+			}
+
+			
+
+	
+	
+
+}
