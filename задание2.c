002fd7eb2b9795f5d51b729fb2876679a54b9cb4
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-12-09 00:49:52 +0300

    Update задание2.c

--- задание2.c
+++ задание2.c
@@ -1,30 +1,35 @@
-include <stdio.h> 
+#include <stdio.h>
 #include <locale.h>
 #include <math.h> 
 #define N 10
 #define K 100
-int main() {
-	int A[K];
-	int temp;
-	int i, x, k1 = 0, k = 0, k2 = 0;
-	float z = 0, xx = 0, x = 2, F;
+float main() {
+	int i;
+	float z = 0;
+	float xx = 0;
+	float x = 2;
 	double gg = 0;
+	int k1 = 0;
+	int k = 0;
+	int k2 = 0;
+	float M[K];
+	float F;
 	setlocale(0, "rus");
-	puts("Задна функция у = 1.8x^2 - sin10x и интервал[0.2;2.2]\n");
-		    scanf("%d", &temp);
-			for (int i = ; i < K; i++) {
-				A[i] = 1.8 * (float)pow(x, 2) - (float)sin(10 * x);
-				if (A[(int)x] >= 0) {
-					z += A[(int)x];
-					k1 += 1;
-				}
-				else{
-				xx += A[(int)x];
-				k2 += 1;
-				}
-				}x += F;
-				gg = z/100;
-				printf("кол-во положиительных чисел =%d чисел, кол-во отрицательных чисел =%d, имя массива: A, ср.знач=%lf\n", k1, k2, gg);
+	printf("Задание функция y=1.8x^2-sin10x и интервал [0.2; 2.2]\n");
+	scanf("%d", &F);
+	for (int j = 0; j < K; j++) {
+		M[j] = 1.8 * (float)pow(x, 2) - (float)sin(10 * x);
+		if (M[(int)x] >= 0) {
+			z += M[(int)x];
+			k1 += 1;
+		}
+		else {
+			xx += M[(int)x];
+			k2 += 1;
+		}
+	} x += F;
+	gg = z / 100;
+	printf("кол-во положиительных чисел =%d чисел, кол-во отрицательных чисел =%d, имя массива: M, ср.знач=%lf\n", k1, k2, gg);
 	return 0;
 }
 				
@@ -35,4 +40,4 @@ int main() {
 	
 	
 
-}
+
