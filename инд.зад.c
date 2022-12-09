e1fbc8d425dece65e4331fd65e77413120a8ff15
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-12-09 12:57:05 +0300

    Update инд.зад.c

--- инд.зад.c
+++ инд.зад.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h> 
+#define n 10
 
 main() {
-	int i, n, temp;
+	int i,  temp, a[n];
 	float s = 0;
 	float m;
-	
+
 	setlocale(0, "");
-	scanf("%d", &n);
 	
+
 	for (int i = 0; i < n; i++)
 	{
 		printf("a[%d] = ", i + 1);
 
 		scanf("%d", &temp);
-		
 		a[i] = temp;
+
 	}
+
 	for (int i = 0; i < n; i++) {
 
 		if (i % 2 != 0 && a[i] > 0) {
@@ -27,6 +29,7 @@ main() {
 			m = s / n;
 		}
 
-		
+
 	}
 	printf("Среднее значение положительных элементов массива, по нечётным я чейком = %.2f", m);
+}
