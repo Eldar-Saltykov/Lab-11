a7f1de3366c5991070a928900150d055d99b8cc8
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-12-09 13:49:28 +0300

    Update инд.зад.c

--- инд.зад.c
+++ инд.зад.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h> 
-#define n 10
-
+#include <stdlib.h>
+#define N 100
+#define XMAX 112.4
+#define NMAX 10
+#define XMIN -100
 main() {
-	int i,  temp, a[n];
+	int i, temp, a[N];
 	float s = 0;
 	float m;
+	int stime;
+	long ltime;
+	ltime = time(NULL);
+	stime = (unsigned)ltime / 2;
+	srand(stime);
 
 	setlocale(0, "");
-	
 
-	for (int i = 0; i < n; i++)
-	{
-		printf("a[%d] = ", i + 1);
 
-		scanf("%d", &temp);
-		a[i] = temp;
+	for (i = 0; i < N; i++) {
 
+		a[i] = XMIN + 1.f * (XMAX - XMIN) * rand() / RAND_MAX;
+		printf("a[%d] = %d\n", i, a[i]);
 	}
 
-	for (int i = 0; i < n; i++) {
+
+	for (i = 0; i < N; i++) {
 
 		if (i % 2 != 0 && a[i] > 0) {
 
 
 			s = s + a[i];
-			m = s / n;
+			m = s / N;
 		}
 
 
 	}
-	printf("Среднее значение положительных элементов массива, по нечётным я чейком = %.2f", m);
+	printf("Среднее значение положительных элементов массива, по нечётным ячейкам = %.2f", m);
 }
